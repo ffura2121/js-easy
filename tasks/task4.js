cc6ce5d26b4875c40359adc4bc1af254b467d546
ffura2121 <153360106+ffura2121@users.noreply.github.com>
2024-10-05 15:20:54 +0300

    Update task4.js

--- tasks/task4.js
+++ tasks/task4.js
@@ -1,7 +1,19 @@
 // Задача: Напишіть функцію findMax, яка приймає масив чисел і повертає найбільше число в масиві.
 
 function findMax(numbers) {
-  //Ваш код
+if (arr.length === 0) {
+    return undefined;
+  }
+
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
 }
 
 // Приклади використання:
